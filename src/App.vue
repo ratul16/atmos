c0632204844979c101a3236361d16b1e48f4f3b7
Md. Hasibul Alam Ratul <hasibul.ratul@northsouth.edu>
2020-05-29 12:48:59 +0600

    atmos app added

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" >
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Weather/>
+  </div>
+</template>
+
+<script>
+import Weather from './components/Weather'
+
+export default {
+  name: 'App',
+  data:()=>({
+    temp :''
+  }),
+  components: {
+    Weather
+  },
+}
+</script>
+
+<style>
+@import './assets/style.css';
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
