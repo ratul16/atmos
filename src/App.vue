0d17aef082cb3a013d2d7aa7ec2f9c14f70431ae
Md. Hasibul Alam Ratul <hasibul.ratul@northsouth.edu>
2022-07-15 09:27:00 +0600

    router added code nit

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,13 @@
 <template>
   <div id="app">
-    <a href="https://github.com/ratul16/atmos" target="_blank" class="btn btn-dark repo shadow-lg">
-      <i class="fab fa-github"></i>
-    </a>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <Weather/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Weather from './components/Weather'
-
-export default {
-  name: 'App',
-  data:()=>({
-    temp :''
-  }),
-  components: {
-    Weather
-  },
-}
-</script>
-
 <style>
 @import './assets/style.css';
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-
-.repo {
-  position: fixed;
-  bottom: 2%;
-  right: 5%;
-  width: 60px;
-  height: 60px;
-  padding: 9px;
-  border-radius: 50%;
-  text-align: center;
-  z-index: 99999;
-}
-
-.repo:hover{
-  transform: scale(1.1);
-  transition: .5s;
-}
-
-.repo i {
-  font-size: 2.5em;
-}
 </style>
